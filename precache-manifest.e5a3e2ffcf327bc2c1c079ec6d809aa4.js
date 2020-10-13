c52533591eecafbeac4784e35583e88ca18c26b1
Eka Prasetia <ekaone3033@gmail.com>
2020-10-13 08:19:26 +0700

    Updates

---
+++ precache-manifest.e5a3e2ffcf327bc2c1c079ec6d809aa4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be46cb7ddc2f4faf74e5e4b1de3ec0d4",
+    "url": "/Mock-API/index.html"
+  },
+  {
+    "revision": "b7130224294d87597ecf",
+    "url": "/Mock-API/static/css/2.c4361303.chunk.css"
+  },
+  {
+    "revision": "b7130224294d87597ecf",
+    "url": "/Mock-API/static/js/2.e07f38d2.chunk.js"
+  },
+  {
+    "revision": "11fec92740e25d13af3ae87a498b3286",
+    "url": "/Mock-API/static/js/2.e07f38d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "326b3331909d994fed31",
+    "url": "/Mock-API/static/js/main.a1e368d9.chunk.js"
+  },
+  {
+    "revision": "fd307b2ce2808b69d315",
+    "url": "/Mock-API/static/js/runtime-main.3ba14184.js"
+  }
+]);
